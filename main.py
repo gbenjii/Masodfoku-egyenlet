676687be35cb7c5dde0aa59af28645aaecabeef4
Benji <133044921+gbenjii@users.noreply.github.com>
2026-01-12 23:09:03 +0100

    Implement quadratic equation solver with discriminant
    The program calculates the discriminant of a quadratic equation and determines the number of real solutions based on its value. It includes input validation for coefficients A, B, and C.

---
+++ main.py
@@ -0,0 +1,54 @@
+import time
+import math
+
+print("ez az alkalmazás képes megoldani egy másodfokú egyenletet diszkrimináns számolással")
+
+time.sleep(1)
+while True:
+    a_szam = input('Kérem az A számot: ')
+    try:
+        a_szam = int(a_szam)
+        break
+    except ValueError:
+        print("Ez nem egy egész szám! Próbáld Újra!")
+        
+while True:        
+    b_szam = input("Kérem az B számot: ")
+    try:
+        b_szam = int(b_szam)
+        break
+    except ValueError:
+        print("Ez nem egy egész szám! Próbáld Újra!")
+        
+while True:
+    c_szam = input("Kérem az C számot: ")
+    try:
+        c_szam = int(c_szam)
+        break
+    except ValueError:
+        print("Ez nem egy egész szám! Próbáld Újra!")
+
+diszkriminans = (b_szam*b_szam-4*a_szam*c_szam)
+
+print("A diszkrimináns:","|",diszkriminans,"|")
+
+if diszkriminans < 0:
+    print("Nincs valós megoldás.")
+    exit()
+else:
+    diszkriminans > 0
+    gyokszar = math.sqrt(diszkriminans)
+    
+(b_szam) = b_szam
+
+if diszkriminans > 0:
+    print("Kettő valós megoldás van.")
+    x1 = ((-(b_szam))+gyokszar)/(2*a_szam)
+    x2 = (((-b_szam))-gyokszar)/(2*a_szam)
+    print("x1",x1)
+    print("x2",x2)
+else:
+    diszkriminans == 0
+    print("Egy valós megoldás van.")
+    x1= (((-b_szam))+gyokszar)/(2*a_szam)
+    print(x1)
